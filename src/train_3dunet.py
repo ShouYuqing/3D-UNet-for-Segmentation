8a9d7983490bfa08482dc624ca96f14ca713613d
yuqing <573380218@qq.com>
2018-10-11 17:18:52 -0400

    Update src/train_3dunet.py

--- src/train_3dunet.py
+++ src/train_3dunet.py
@@ -30,6 +30,6 @@ vol_size = (160, 192, 224)
 
 vol_data_dir='/home/ys895/resize256/resize256-crop_x32/train/vols/'
 seg_data_dir='/home/ys895/resize256/resize256-crop_x32/train/asegs/'
-a,b=genera.vol_seg(vol_data_dir,seg_data_dir,nb_labels_reshape =30)
+a,b=genera.vol_seg(vol_data_dir,seg_data_dir,nb_labels_reshape =50)
 
 #random.shuffle(train_vol_names)
