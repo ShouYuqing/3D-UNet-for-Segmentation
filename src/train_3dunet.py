8ddc3f4f4d28f6060ec27fd4f24e089bff1e5adb
yuqing <573380218@qq.com>
2018-10-11 17:16:29 -0400

    Update src/train_3dunet.py

--- src/train_3dunet.py
+++ src/train_3dunet.py
@@ -17,7 +17,7 @@ sys.path.append('../ext/medipy-lib')
 sys.path.append('../ext/pynd-lib')
 sys.path.append('../ext/pytools-lib')
 import datagenerators
-#import networks
+import networks
 import losses
 import neuron.generators as genera
 
@@ -30,6 +30,6 @@ vol_size = (160, 192, 224)
 
 vol_data_dir='/home/ys895/resize256/resize256-crop_x32/train/vols/'
 seg_data_dir='/home/ys895/resize256/resize256-crop_x32/train/asegs/'
-a,b=genera.vol_seg(vol_data_dir,seg_data_dir)
+a,b=genera.vol_seg(vol_data_dir,seg_data_dir,nb_labels_reshape =30)
 
 #random.shuffle(train_vol_names)
