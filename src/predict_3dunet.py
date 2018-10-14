34ebe31eaab1ca8da774956546cdd2de1c641fbd
yuqing <573380218@qq.com>
2018-10-14 16:05:10 -0400

    amend

--- src/predict_3dunet.py
+++ src/predict_3dunet.py
@@ -10,7 +10,8 @@ import numpy as np
 import keras
 from keras.models import load_model, Model
 import tensorflow as tf
-
+sys.path.append('../ext/neuron')
+import neuron.plot as nplt
 import unet_models as un
 
 # read labels
@@ -18,16 +19,27 @@ rl_data = sio.loadmat('labels.mat')
 l_data = rl_data['labels']
 labels_data = l_data[0]
 
+# read data
+vol_dir='../data/'
+atlas = np.load('../data/atlas_norm.npz')
+atlas_vol = atlas['vol']
+atlas_seg = atlas['seg']
+
+# slice the image
+slice_vol = atlas_vol[100]
+slice_seg = atlas_seg[100]
 # vol_size
 vol_size = (160, 192 ,224)
 
 # set model directory
-#m_dir = '../models/slice20_30.h5'
+m_dir = '../models/slice100_30.h5'
+
 
 # set model
-#model = un.unet(pretrained_weights = m_dir, label_nums = len(labels_data))
+load_model = un.unet(pretrained_weights = m_dir, label_nums = len(labels_data))
 
-# slice the image
+# predict
+p_outcome = load_model.predict(atlas_vol)
 
 
 # plot the image
