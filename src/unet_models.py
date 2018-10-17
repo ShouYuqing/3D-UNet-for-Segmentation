d62568ea59c80bd422a47178162a026737d1bc75
yuqing <573380218@qq.com>
2018-10-16 22:46:29 -0400

    amend

--- src/unet_models.py
+++ src/unet_models.py
@@ -20,20 +20,25 @@ input in (1,x,y,1) 4d tensor
 def unet(pretrained_weights=None, input_size=(256, 256, 1), label_nums=1):
     inputs = Input(input_size)
     conv1 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Dropout(0.2)(conv1)
     conv1 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
     conv2 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = Dropout(0.2)(conv2)
     conv2 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
     conv3 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = Dropout(0.2)(conv3)
     conv3 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     conv4 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = Dropout(0.2)(conv4)
     conv4 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
     conv5 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Dropout(0.2)(conv5)
     conv5 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
@@ -42,6 +47,7 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1), label_nums=1):
     merge6 = concatenate([drop4, up6], axis=3)
     # merge6 = merge([drop4,up6], mode = 'concat', concat_axis = 3)
     conv6 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv16 = Dropout(0.2)(conv6)
     conv6 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
     up7 = Conv2D(32, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
@@ -49,6 +55,7 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1), label_nums=1):
     merge7 = concatenate([conv3, up7], axis=3)
     # merge7 = merge([conv3,up7], mode = 'concat', concat_axis = 3)
     conv7 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = Dropout(0.2)(conv7)
     conv7 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
     up8 = Conv2D(32, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
@@ -56,6 +63,7 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1), label_nums=1):
     merge8 = concatenate([conv2, up8], axis=3)
     # merge8 = merge([conv2,up8], mode = 'concat', concat_axis = 3)
     conv8 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = Dropout(0.2)(conv8)
     conv8 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
     up9 = Conv2D(16, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
@@ -63,6 +71,7 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1), label_nums=1):
     merge9 = concatenate([conv1, up9], axis=3)
     # merge9 = merge([conv1,up9], mode = 'concat', concat_axis = 3)
     conv9 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = Dropout(0.2)(conv9)
     conv9 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv9 = Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     ## conv10 = Conv2D(,50, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
