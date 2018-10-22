edda597379d2d8a7e3d5b90cbd08ab3b4eecbf30
yuqing <573380218@qq.com>
2018-10-22 17:44:15 -0400

    Update src/test_3dunet2.py

--- src/test_3dunet2.py
+++ src/test_3dunet2.py
@@ -17,12 +17,13 @@ import tensorflow as tf
 sys.path.append('../ext/neuron')
 sys.path.append('../ext/pynd-lib')
 sys.path.append('../ext/medipy-lib')
+import neuron.metrics as nm
 import neuron.plot as nplt
 import unet_models as un
 import neuron.generators as genera
 
 # number of iteration for testing
-iter_num = 1000
+iter_num = 20
 test_iter = 500
 # data direction
 vol_data_dir='/home/ys895/resize256/resize256-crop_x32/train/vols/'
@@ -42,15 +43,21 @@ load_model = un.unet(pretrained_weights = m_dir, input_size=vol_size1, label_num
 # test the model with 3D images
 for (vol_data,seg_data) in genera.vol_seg(vol_data_dir, seg_data_dir,relabel=labels_data,  nb_labels_reshape=len(labels_data),
                                                    iteration_time=iter_num):
+    concatenate_outcome = np.empty(seg_data.shape)
     for i in range(0,191):
         vol_train = vol_data[:, :, i, :, :]
 
         # concatenate slices
-        concatenate_outcome = load_model.predict(slice_vol)
-        concatenate_outcome = np.concatenate([concatenate_outcome,concatenate_outcome])
+        #slice_outcome = load_model.predict(slice_vol)
+        concatenate_outcome[:,:,i,:,:] = load_model.predict(slice_vol)
+        #np.concatenate([concatenate_outcome,concatenate_outcome])
 
-    concatenate_outcome.reshape([1,concatenate_outcome.shape[],concatenate_outcome.shape[0],concatenate_outcome.shape[2],concatenate_outcome.shape[3]])
-
+    concatenate_outcome.reshape([1,concatenate_outcome.shape[1],concatenate_outcome.shape[0],concatenate_outcome.shape[2],concatenate_outcome.shape[3]])
+    print('the shape of the output:')
+    print(concatenate_outcome.shape)
+    # compute the dice score of test example
+    print('the dice score of the test is:')
+    #print(nm.Dice().dice())
 
 
 
