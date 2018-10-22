76753388908b3fec0a3632245053066080a219be
yuqing <573380218@qq.com>
2018-10-22 17:25:46 -0400

    Update src/test_3dunet2.py

--- src/test_3dunet2.py
+++ src/test_3dunet2.py
@@ -22,8 +22,8 @@ import unet_models as un
 import neuron.generators as genera
 
 # number of iteration for testing
-test_num = 1000
-
+iter_num = 1000
+test_iter = 500
 # data direction
 vol_data_dir='/home/ys895/resize256/resize256-crop_x32/train/vols/'
 seg_data_dir='/home/ys895/resize256/resize256-crop_x32/train/asegs/'
@@ -33,14 +33,34 @@ rl_data = sio.loadmat('../data/labels.mat')
 l_data = rl_data['labels']
 labels_data = l_data[0]
 
-# construct the 3D images
-
 # set model directory
-m_dir = '/home/ys895/Models/iter' + test_num + '.h5'
+m_dir = '/home/ys895/Models/iter' + test_iter + '.h5'
 
 # load weight
 load_model = un.unet(pretrained_weights = m_dir, input_size=vol_size1, label_nums = len(labels_data))
 
 # test the model with 3D images
-for i in range(0,1000):
-    for (vol_data,seg_data) in genera.seg_vol()
+for (vol_data,seg_data) in genera.vol_seg(vol_data_dir, seg_data_dir,relabel=labels_data,  nb_labels_reshape=len(labels_data),
+                                                   iteration_time=iter_num):
+    for i in range(0,191):
+        vol_train = vol_data[:, :, i, :, :]
+
+        # concatenate slices
+        concatenate_outcome = load_model.predict(slice_vol)
+        concatenate_outcome = np.concatenate([concatenate_outcome,concatenate_outcome])
+
+    concatenate_outcome.reshape([1,concatenate_outcome.shape[],concatenate_outcome.shape[0],concatenate_outcome.shape[2],concatenate_outcome.shape[3]])
+
+
+
+
+"""
+2d unet changed for class project
+output in (1,256,256,label_nums)
+input in (1,x,y,1) 4d tensor
+"""
+
+'''
+we need to change the output into (1, 256, 256, label_nums)
+'''
+
