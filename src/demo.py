64d74929f88ca5d7979196d1f4de36cf5bf41559
yuqing <573380218@qq.com>
2018-10-26 15:47:29 -0400

    Update src/demo.py

---
+++ src/demo.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import sys
+import random
+from argparse import ArgumentParser
+import scipy.io as sio
+import numpy as np
+
+import keras
+from keras.models import load_model, Model
+import tensorflow as tf
+sys.path.append('../ext/neuron')
+sys.path.append('../ext/pynd-lib')
+sys.path.append('../ext/medipy-lib')
+import neuron.metrics as nm
+import neuron.plot as nplt
+import unet_models as un
+import neuron.generators as genera
+from medipy.metrics import dice
+
+
+'''
+demo for function dice score
+'''
+
+
+tensor1 = np.array([[[0,1,0],[0,1,0],[0,1,0]],[[0,1,0],[0,1,0],[0,1,0]],[[0,1,0],[0,1,0],[0,1,0]]])
+tensor2 = np.array([[[0,1,0],[0,1,0],[0,1,0]],[[0,1,0],[0,1,0],[0,1,1]],[[0,1,0],[0,1,0],[0,1,0]]])
+
+vals, _ = dice(tensor1, tensor2, nargout=2)
+print('calcualting dice score:')
+print(np.mean(vals), np.std(vals))
