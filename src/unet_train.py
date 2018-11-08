9859d371379c544238812907be4f5d6632c6725d
yuqing <573380218@qq.com>
2018-11-08 18:40:28 -0500

    Update src/unet_train.py

--- src/unet_train.py
+++ src/unet_train.py
@@ -40,9 +40,9 @@ seg_list = list() # list of segmentation data
 for i in range(0,lenn):
     st = train_strings[i]
     #print(st.strip())
-    vol_dir = '/home/ys895/resize256/resize256-crop_x32/FromEugenio_prep/vols/' + st.strip()
+    vol_dir = ('/home/ys895/resize256/resize256-crop_x32/FromEugenio_prep/vols/' + st.strip())[0]
     seg_dir = '/home/ys895/resize256/resize256-crop_x32/FromEugenio_prep/labels/' + st.strip()
-    print(vol_dir)
+    #print(vol_dir)
     #train_add = np.load(st.strip())
     X_vol, X_seg = datagenerators.load_example_by_name(vol_dir, seg_dir)
     #train_add = np.reshape(train_add,(1,)+train_add.shape+(1,))
