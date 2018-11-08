6aaaceb63f419466df8d64ee640794c344fcdc5b
yuqing <573380218@qq.com>
2018-11-08 17:36:42 -0500

    Update src/unet_train.py

--- src/unet_train.py
+++ src/unet_train.py
@@ -25,11 +25,11 @@ sys.path.append('../ext/neuron')
 sys.path.append('../ext/medipy-lib')
 sys.path.append('../ext/pynd-lib')
 sys.path.append('../ext/pytools-lib')
-import losses
+#import losses
 import neuron.generators as genera
 
 #import my library
-import unet_models as un
+#import unet_models as un
 
 # read train data
 train_file = open('../data/train_data.txt')
@@ -40,7 +40,7 @@ seg_list = list() # list of segmentation data
 for i in range(0,lenn):
     st = train_strings[i]
     #train_add = np.load(st.strip())
-    X_vol, X_seg = datagenerators.load_example_by_name(
+    X_vol, X_seg = genera.load_example_by_name(
         '/home/ys895/resize256/resize256-crop_x32/FromEugenio_prep/vols/' + st.strip(),
         '/home/ys895/resize256/resize256-crop_x32/FromEugenio_prep/labels/' + st.strip())
     #train_add = np.reshape(train_add,(1,)+train_add.shape+(1,))
