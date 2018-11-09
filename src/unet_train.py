94b5c3857f7c39ad193d0bf9da21ea9f13e95df5
yuqing <573380218@qq.com>
2018-11-08 22:23:42 -0500

    Update src/unet_train.py

--- src/unet_train.py
+++ src/unet_train.py
@@ -60,6 +60,8 @@ for i in range(0, 1):
     rand_num = random.randint(0, 18)
     X_vol = vol_list[rand_num]
     X_seg = seg_list[rand_num]
-    for [vol,seg] in [genera.patch(X_vol[0,:,:,:,0], patch_size = [64,64,64],patch_stride=32), genera.patch(X_seg[0,:,:,:,0], patch_size = [64,64,64],patch_stride=32)]:
-        print(vol.size)
+    vol_p = genera.patch(X_vol[0, :, :, :, 0], patch_size=[64, 64, 64], patch_stride=32)
+    seg_p = genera.patch(X_seg[0, :, :, :, 0], patch_size=[64, 64, 64], patch_stride=32)
+    #for [vol,seg] in [genera.patch(X_vol[0,:,:,:,0], patch_size = [64,64,64],patch_stride=32), genera.patch(X_seg[0,:,:,:,0], patch_size = [64,64,64],patch_stride=32)]:
+    #   print(vol.size)
 
