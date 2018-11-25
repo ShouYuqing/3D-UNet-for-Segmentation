5fd94729f94da08495af5c8b5b9112b8110b85f9
yuqing <573380218@qq.com>
2018-11-25 00:47:56 -0500

    Update src/unet_train.py

--- src/unet_train.py
+++ src/unet_train.py
@@ -67,7 +67,6 @@ for i in range(0, 1):
     X_seg = seg_list[rand_num]
     for vol,arg in palib.patch_gen(X_vol[0, :, :, :, 0], patch_size=[64, 64, 64], stride=32, nargout=0):
     #for vol in genera.patch(X_vol[0, :, :, :, 0], patch_size=[64, 64, 64], patch_stride=32):
-        print(vol.shape)
         arg_arr = np.array(arg)
         # get segmentation data
         seg=X_seg[:,arg_arr[0], arg_arr[1], arg_arr[2],:]
@@ -75,6 +74,9 @@ for i in range(0, 1):
         # adjust data
         vol = np.reshape(vol, (1,) + vol.shape + (1,))
         #seg = np.reshape(seg, (1,) + vol.shape + (1,))
+        # adjust segmentation data into  one-hot encoding
+
+
         # train
         print('volume ' + str(i) + 'training...')
         model.fit(vol, seg, batch_size=1)
