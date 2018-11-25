cc2301348f0cfee26d6b93762a93c25cc2946f12
yuqing <573380218@qq.com>
2018-11-24 19:12:48 -0500

    amend

--- src/unet_train.py
+++ src/unet_train.py
@@ -63,8 +63,10 @@ for i in range(0, 1):
     X_seg = seg_list[rand_num]
     for vol,arg in palib.patch_gen(X_vol[0, :, :, :, 0], patch_size=[64, 64, 64], stride=32, nargout=0):
     #for vol in genera.patch(X_vol[0, :, :, :, 0], patch_size=[64, 64, 64], patch_stride=32):
-        print(vol.shape)
-        arg_arr=np.array(arg)
+        arg_arr = np.array(arg)
+        seg=X_seg[arg_arr[0], arg_arr[1], arg_arr[2]]
+        print(seg.shape)
+        
         #print(arg.shape)
         print(arg_arr)
         print(arg_arr.shape)
