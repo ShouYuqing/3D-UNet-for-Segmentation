077720496e000e0618349f07f13a2f010a84f7d7
yuqing <573380218@qq.com>
2018-11-26 02:07:56 -0500

    Update src/unet_test.py

--- src/unet_test.py
+++ src/unet_test.py
@@ -34,8 +34,9 @@ import unet_models as un
 
 
 # read model
-test_iter =
+test_iter = 50
 m_dir = '/home/ys895/unet/iter' + str(test_iter) + '.h5'
+
 
 # read validation data
 valid_file = open('../data/validate_data.txt')
@@ -52,7 +53,25 @@ for i in range(0,lenn):
     seg_list.append(X_seg)
 
 # test the data on the model
-
+cnt = 1
+for i in range(0, 19 * 3):
+    # rand_num = random.randint(0, 18)
+    ii = i % 19
+    X_vol = vol_list[ii]
+    X_seg = seg_list[ii]
+    for vol, arg in palib.patch_gen(X_vol[0, :, :, :, 0], patch_size=[64, 64, 64], stride=32, nargout=0):
+        arg_arr = np.array(arg)
+        # get segmentation data
+        seg = X_seg[0, arg_arr[0], arg_arr[1], arg_arr[2], 0]
+        seg = genera._relabel(seg, labels=labels)
+        # print(seg.shape)
+        seg = seg.astype(np.int64)
+        seg = genera._categorical_prep(seg, nb_labels_reshape=31, keep_vol_size=True, patch_size=[64, 64, 64])
+        # seg = metrics._label_to_one_hot(seg, nb_labels=31)
+        # print(seg.shape)
+        # seg = seg.reshape((1,)+ seg.shape +(1,))
+        # adjust data
+        vol = np.reshape(vol, (1,) + vol.shape + (1,))
 
 
 #
